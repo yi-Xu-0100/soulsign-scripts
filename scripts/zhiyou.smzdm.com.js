4a1985af0b549499d0cec1eb9d630c5eec7dc690
yi_Xu <xjy123456120@163.com>
2020-07-18 11:48:27 +0800

    更新什么值得买的签到脚本到 V1.0.1（个人修改版）

--- scripts/zhiyou.smzdm.com.js
+++ scripts/zhiyou.smzdm.com.js
@@ -1,21 +1,21 @@
 // ==UserScript==
 // @name              什么值得买
-// @namespace         https://soulsign.inu1255.cn?account=inu1255
-// @version           1.0.0
-// @author            inu1255
+// @namespace         https://soulsign.inu1255.cn/scripts/206
+// @version           1.0.1
+// @author            yi-Xu-0100
 // @loginURL          https://zhiyou.smzdm.com/user/login
-// @updateURL         https://soulsign.inu1255.cn/script/inu1255/什么值得买
+// @updateURL         https://soulsign.inu1255.cn/script/yi-Xu-0100/什么值得买
 // @expire            900000
 // @domain            zhiyou.smzdm.com
 // ==/UserScript==
 
-exports.run = async function () {
+exports.run = async function (param) {
     var { data } = await axios.get('https://zhiyou.smzdm.com/user/checkin/jsonp_checkin', { headers: { 'Referer': 'https://www.smzdm.com/' } });
-    if (data.error_code == 0) return '签到成功';
-    throw data.error_msg || "失败";
+    if (data.error_code == 0) return '已连续签到 ' + data.data.checkin_num + " 天";
+    throw data.error_msg || "签到失败";
 };
 
-exports.check = async function () {
-    var { data } = await axios.get('https://zhiyou.smzdm.com/user/checkin/jsonp_checkin', { headers: { 'Referer': 'https://www.smzdm.com/' } });
-    return data.error_code == 0;
+exports.check = async function (param) {
+    var { data } = await axios.get('https://zhiyou.smzdm.com/user/', { headers: { 'Referer': 'https://www.smzdm.com/' } });
+    return /个人中心 \| 什么值得买/.test(data);
 };
