4e0966a6f89d0131e75653cc5c32357b660f84ef
yi_Xu <xjy123456120@163.com>
2020-08-19 09:40:29 +0800

    修复 conf 和百度文库签到脚本

--- scripts/wenku.baidu.com.js
+++ scripts/wenku.baidu.com.js
@@ -19,9 +19,8 @@
 /**
  * @module 百度文库签到脚本
  * @description 本脚本借鉴 [inu1255 的百度文库签到脚本](https://soulsign.inu1255.cn/scripts/15)，更改了检查在线逻辑。
- * 
- *              + 脚本内容讨论请转至：[仓库 issue](https://github.com/yi-Xu-0100/soulsign-scripts/issues)
- *              + 签到插件讨论请转至：[官方 issue](https://github.com/inu1255/soulsign-chrome/issues)
+ * @description 脚本内容讨论请转至：[仓库 issue](https://github.com/yi-Xu-0100/soulsign-scripts/issues)
+ * @description 签到插件讨论请转至：[官方 issue](https://github.com/inu1255/soulsign-chrome/issues)
  * @param {string|string[]} [domain = wenku.baidu.com] - 请求的域名
  * @param {string} [expire = 900000] - 在线检查频率
  * @param {string} [namespace = https://soulsign.inu1255.cn/scripts/239] - 脚本主页
