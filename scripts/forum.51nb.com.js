f72d035d46a9d03a5e8bd3b1ef302cabc1a0c30a
yi_Xu <xjy123456120@163.com>
2020-09-06 08:46:33 +0800

    更新相关论坛签到脚本为通用模板 + 升级51NB论坛签到脚本到V1.0.4 + 创建人大经济论坛签到脚本到V1.0.1

--- scripts/forum.51nb.com.js
+++ scripts/forum.51nb.com.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              51NB论坛
 // @namespace         https://soulsign.inu1255.cn/scripts/248
-// @version           1.0.2
+// @version           1.0.4
 // @author            yi-Xu-0100
 // @loginURL          https://forum.51nb.com/member.php?mod=logging&action=login
 // @updateURL         https://soulsign.inu1255.cn/script/yi-Xu-0100/51NB论坛
@@ -13,12 +13,12 @@
  * @file 51NB论坛签到脚本
  * @author yi-Xu-0100
  * @author hithy123
- * @version 1.0.2
+ * @version 1.0.4
  */
 
 /**
  * @module 51NB论坛签到脚本
- * @description 本脚本借鉴 [hithy123 的人大经济论坛签到脚本](https://soulsign.inu1255.cn/scripts/225)，更改了检查在线逻辑。
+ * @description 本脚本借鉴 [hithy123 的人大经济论坛签到脚本](https://soulsign.inu1255.cn/scripts/225)，更改了检查在线逻辑，设置了通用模板格式。
  * 
  * 脚本内容讨论请转至：[仓库 issue](https://github.com/yi-Xu-0100/soulsign-scripts/issues)
  * 
@@ -30,13 +30,15 @@
  * @param {string} [updateURL = https://soulsign.inu1255.cn/script/yi-Xu-0100/51NB论坛] - 脚本更新链接
  */
 
+var domainurl = "https://forum.51nb.com/";
+
 exports.run = async function (param) {
-    var signurl = "https://forum.51nb.com/plugin.php?id=dsu_paulsign:sign";
+    var signurl = domainurl + "plugin.php?id=dsu_paulsign:sign";
     var { data } = await axios.get(signurl);
-    if(/需要先登录/.test(data)) throw "需要登录";
+    if (/需要先登录/.test(data)) throw "需要登录";
     if (/已经签到/.test(data)) return "已经签到";
     var formhash = /name="formhash" value="([^"]+)/.exec(data)[1];
-    var { data } = await axios.post(signurl + '&operation=qiandao&infloat=1&inajax=1',
+    var { data } = await axios.post(signurl + '&operation=qiandao&infloat=1&inajax=1&sign_as=1',
         `formhash=${formhash}&qdxq=kx`);
     if (/已经签到/.test(data)) return '重复签到';
     var { data } = await axios.post(signurl);
@@ -45,6 +47,6 @@ exports.run = async function (param) {
 };
 
 exports.check = async function (param) {
-    var { data } = await axios.get('https://forum.51nb.com/home.php?mod=spacecp');
+    var { data } = await axios.get(domainurl + 'home.php?mod=spacecp');
     return !(/先登录/.test(data));
 };
