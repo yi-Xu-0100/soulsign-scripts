7a5528b4992fadd7a019ce8c773ae648d8ed1fc0
yi_Xu <xjy123456120@163.com>
2020-11-11 09:01:45 +0800

    📝 更新白嫖云签到脚本到V1.0.1

--- scripts/baipiaoyun.xyz.js
+++ scripts/baipiaoyun.xyz.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name              白嫖云签到
 // @namespace         https://soulsign.inu1255.cn/scripts/293
-// @version           1.0.0
+// @version           1.0.1
 // @author            yi-Xu-0100
-// @loginURL          https://portal.nonsense.cloud/auth/login
+// @loginURL          https://baipiaoyun.xyz/auth/login
 // @updateURL         https://soulsign.inu1255.cn/script/yi-Xu-0100/白嫖云签到
 // @expire            900000
-// @domain            portal.nonsense.cloud
+// @domain            baipiaoyun.xyz
 // @param            name 账户
 // @param            pwd 密码
 // ==/UserScript==
@@ -15,7 +15,7 @@
  * @file 白嫖云签到脚本
  * @author yi-Xu-0100
  * @author marvolo666
- * @version 1.0.0
+ * @version 1.0.1
  */
 
 /**
@@ -25,7 +25,7 @@
  * 脚本内容讨论请转至：[仓库 issue](https://github.com/yi-Xu-0100/soulsign-scripts/issues)
  *
  * 签到插件讨论请转至：[官方 issue](https://github.com/inu1255/soulsign-chrome/issues)
- * @param {string|string[]} [domain = portal.nonsense.cloud] - 请求的域名
+ * @param {string|string[]} [domain = baipiaoyun.xyz] - 请求的域名
  * @param {string} [expire = 900000] - 在线检查频率
  * @param {string} [namespace = https://soulsign.inu1255.cn/scripts/293] - 脚本主页
  * @param {string} [loginURL = https://portal.nonsense.cloud/auth/login] - 登录链接
@@ -35,7 +35,7 @@
  */
 
 exports.run = async function (param) {
-  var resp = await axios.post('https://portal.nonsense.cloud/user/checkin');
+  var resp = await axios.post('https://baipiaoyun.xyz/user/checkin');
   if (/成功/.test(resp.data.msg)) return resp.data.msg;
   else if (/您似乎已经签到过了/.test(resp.data.msg)) return '重复签到';
   else if (/获得了 \d+MB 流量/.test(resp.data.msg)) return resp.data.msg;
@@ -43,10 +43,10 @@ exports.run = async function (param) {
 };
 
 exports.check = async function (param) {
-  var resp = await axios.get('https://portal.nonsense.cloud/user');
+  var resp = await axios.get('https://baipiaoyun.xyz/user');
   if (/用户中心/.test(resp.data)) return true;
   else {
-    let resp = await axios.post('https://portal.nonsense.cloud/auth/login', {
+    let resp = await axios.post('https://baipiaoyun.xyz/auth/login', {
       email: param.name,
       passwd: param.pwd,
       remember_me: 'week'
