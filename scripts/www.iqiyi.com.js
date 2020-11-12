a0d4888ed323d59198fd5c89d09548538d2d9001
yi_Xu <xjy123456120@163.com>
2020-11-12 09:26:02 +0800

    📝 更新爱奇艺签到脚本到V1.0.5

--- scripts/www.iqiyi.com.js
+++ scripts/www.iqiyi.com.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              爱奇艺签到
 // @namespace         https://soulsign.inu1255.cn/scripts/290
-// @version           1.0.4
+// @version           1.0.5
 // @author            yi-Xu-0100
 // @loginURL          https://www.iqiyi.com/u/accountset
 // @updateURL         https://soulsign.inu1255.cn/script/yi-Xu-0100/爱奇艺签到
@@ -15,11 +15,11 @@
 /**
  * @file 爱奇艺签到脚本
  * @author yi-Xu-0100
- * @version 1.0.4
+ * @version 1.0.5
  */
 
 /**
- * @module geekhub签到脚本
+ * @module 爱奇艺签到脚本
  * @description  本脚本借鉴 [NobyDa/Script](https://github.com/NobyDa/Script/blob/42e6e1978fed46f531666d0db096b67858592fda/iQIYI-DailyBonus/iQIYI.js)，需要 cookie 权限获取签到相关参数。
  *
  * 脚本内容讨论请转至：[仓库 issue](https://github.com/yi-Xu-0100/soulsign-scripts/issues)
@@ -51,7 +51,7 @@ let run = async function (param) {
         signInfo.data.signRewardResponses[i].name +
         signInfo.data.signRewardResponses[i].value;
     }
-  else throw signInfo.msg;
+  else message = signInfo.msg;
   var resp1 = await axios.get(
     `https://tc.vip.iqiyi.com/taskCenter/task/joinTask?P00001=${P00001}&taskCode=b6e688905d4e7184&platform=b6c13e26323c537d&lang=zh_CN&app_lm=cn`
   );
@@ -62,14 +62,15 @@ let run = async function (param) {
     );
     if (resp.data.code === 'A00000')
       message = message + '，浏览会员俱乐部，成长值' + (resp.data.data[0]['成长值'] || '未知');
-  } else throw '浏览会员俱乐部出错，' + resp.data.msg;
+  } else message = message + '浏览会员俱乐部出错，' + resp.data.msg;
   var resp2 = await axios.get(
     `https://community.iqiyi.com/openApi/score/add?authCookie=${P00001}&userId=${userId}&channelCode=sign_pcw&agenttype=1&agentversion=0&appKey=basic_pca&appver=0&srcplatform=1&typeCode=point&verticalCode=iQIYI&scoreType=1&user_agent=Mozilla/5.0%20(Windows%20NT%2010.0;%20Win64;%20x64)%20AppleWebKit/537.36%20(KHTML,%20like%20Gecko)%20Chrome/88.0.4298.0%20Safari/537.36%20Edg/88.0.685.3&dfp=${dfp}&sign=2f03d94fa65964342b5c698654ae0f50`
   );
-  if (resp2.data.data[0].code === 'A0002') message = message + '，个人中心重复签到';
-  else if (resp2.data.data[0].code === 'A0000')
+  if (resp2.data.data && resp2.data.data[0].code === 'A0002')
+    message = message + '，个人中心重复签到';
+  else if (resp2.data.data && resp2.data.data[0].code === 'A0000')
     message = message + '，个人中心签到，积分+' + resp2.data.data[0].score;
-  else throw '个人中心签到出错，' + resp2.data.data.message;
+  else message = message + ',个人中心签到出错，' + resp2.data.message;
   var resp3 = await axios.get(
     `https://community.iqiyi.com/openApi/task/complete?authCookie=${P00001}&userId=${userId}&channelCode=paopao_pcw&agenttype=1&agentversion=0&appKey=basic_pcw&appver=0&srcplatform=1&typeCode=point&verticalCode=iQIYI&scoreType=1&sign=d2a03e35a17fbef808146de25829b9a4&callback=cb`
   );
@@ -81,7 +82,7 @@ let run = async function (param) {
     );
     let resp_data = JSON.parse(/cb\((\{[\s\S]*\})\)\}catch/.exec(resp.data)[1]);
     message = message + '，访问热点首页，积分+' + resp_data.data.score;
-  } else throw '访问热点首页出错，' + resp_data.message;
+  } else message = message + '访问热点首页出错，' + resp_data.message;
   return message;
 };
 
