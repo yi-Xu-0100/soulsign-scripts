bccd2fb559a7756c6cd54b8df3958d7af7372365
yi_Xu <xjy123456120@163.com>
2020-11-14 22:05:31 +0800

    📝 更新忍者云签到脚本到 V1.0.9

--- scripts/renzhe.cloud.js
+++ scripts/renzhe.cloud.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              忍者云签到
 // @namespace         https://soulsign.inu1255.cn/scripts/173
-// @version           1.0.8
+// @version           1.0.9
 // @author            yi-Xu-0100
 // @loginURL          https://renzhe.cloud/auth/login
 // @updateURL         https://soulsign.inu1255.cn/script/yi-Xu-0100/忍者云签到
@@ -15,7 +15,7 @@
  * @file 忍者云签到脚本
  * @author yi-Xu-0100
  * @author marvolo666
- * @version 1.0.8
+ * @version 1.0.9
  */
 
 /**
@@ -34,24 +34,30 @@
  * @param {string} pwd - 密码
  */
 
-exports.run = async function (param) {
-  var data = await axios.post('https://renzhe.cloud/user/checkin');
+const _domain = 'renzhe.cloud';
+const _remember_me = 'on';
+
+let run = async function (param) {
+  if (!check(param)) throw '需要登录';
+  var data = await axios.post(`https://${_domain}/user/checkin`);
   if (/成功/.test(data.data.msg)) return data.data.msg;
   else if (/您似乎已经签到过了/.test(data.data.msg)) return '重复签到';
   else if (/获得了 \d{0,4} MB流量/.test(data.data.msg)) return data.data.msg;
-  else throw '检查是否登录';
+  else throw data.data;
 };
 
-exports.check = async function (param) {
-  var { data } = await axios.get('https://renzhe.cloud/user');
+let check = async function (param) {
+  var { data } = await axios.get(`https://${_domain}/user`);
   if (/用户中心/.test(data)) return true;
   else {
-    var data = await axios.post('https://renzhe.cloud/auth/login', {
+    var data = await axios.post(`https://${_domain}/auth/login`, {
       email: param.name,
       passwd: param.pwd,
-      remember_me: 'on'
+      remember_me: _remember_me
     });
     if (/登录成功/.test(data.data.msg)) return true;
     else return false;
   }
 };
+
+module.exports = { run, check };
