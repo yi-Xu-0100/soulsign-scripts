f63be65269431310ece196c3e1342b76eaa24c78
yi_Xu <xjy123456120@163.com>
2020-12-11 09:00:55 +0800

    📝 更新 geekhub 签到脚本到 v1.2.0 修复域名变化造成重定向错误问题

--- scripts/www.geekhub.com.js
+++ scripts/www.geekhub.com.js
@@ -1,18 +1,18 @@
 // ==UserScript==
 // @name              geekhub
 // @namespace         https://soulsign.inu1255.cn/scripts/172
-// @version           1.1.3
+// @version           1.2.0
 // @author            yi-Xu-0100
-// @loginURL          https://geekhub.com/users/sign_in
+// @loginURL          https://www.geekhub.com/users/sign_in
 // @updateURL         https://soulsign.inu1255.cn/script/yi-Xu-0100/geekhub
 // @expire            900000
-// @domain            geekhub.com
+// @domain            www.geekhub.com
 // ==/UserScript==
 
 /**
  * @file geekhub签到脚本
  * @author yi-Xu-0100
- * @version 1.1.3
+ * @version 1.2.0
  */
 
 /**
@@ -20,10 +20,10 @@
  * @description  脚本内容讨论请转至：[仓库 issue](https://github.com/yi-Xu-0100/soulsign-scripts/issues)
  *
  * 签到插件讨论请转至：[官方 issue](https://github.com/inu1255/soulsign-chrome/issues)
- * @param {string|string[]} [domain = geekhub.com] - 请求的域名
+ * @param {string|string[]} [domain = www.geekhub.com] - 请求的域名
  * @param {string} [expire = 900000] - 在线检查频率
  * @param {string} [namespace = https://soulsign.inu1255.cn/scripts/172] - 脚本主页
- * @param {string} [loginURL = https://geekhub.com/users/sign_in] - 登录链接
+ * @param {string} [loginURL = https://www.geekhub.com/users/sign_in] - 登录链接
  * @param {string} [updateURL = https://soulsign.inu1255.cn/script/yi-Xu-0100/geekhub] - 脚本更新链接
  */
 
@@ -43,18 +43,18 @@ function getGbit(data) {
 }
 let run = async function (param) {
   if (!(await check(param))) throw '需要登录';
-  let resp = await axios.get('https://geekhub.com/checkins');
+  let resp = await axios.get('https://www.geekhub.com/checkins');
   if (/今日已签到/.test(resp.data)) return '重复签到';
   let result = resp.data.match(/<meta name="csrf-token" content="(.*?)"/);
   let originGbit = getGbit(resp.data);
   if (originGbit[0]) console.log(`originGbit: ${originGbit[1]}`);
   let resp1 = await axios.post(
-    'https://geekhub.com/checkins/start',
+    'https://www.geekhub.com/checkins/start',
     '_method=post&authenticity_token=' + encodeURIComponent(result[1]),
     {
       headers: {
-        Origin: 'https://geekhub.com',
-        Referer: 'https://geekhub.com/checkins'
+        Origin: 'https://www.geekhub.com',
+        Referer: 'https://www.geekhub.com/checkins'
       }
     }
   );
@@ -67,7 +67,7 @@ let run = async function (param) {
 };
 
 let check = async function (param) {
-  let resp = await axios.get('https://geekhub.com/settings');
+  let resp = await axios.get('https://www.geekhub.com/settings');
   return !/你需要登录后才能继续/.test(resp.data);
 };
 
