7a224821fa40326cecda4d1b832d5570f94d05ef
yi_Xu <xjy123456120@163.com>
2021-05-04 08:54:35 +0800

    fix(script): 🐛 fix unsafe page

--- scripts/music.163.com.js
+++ scripts/music.163.com.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              网易云音乐
 // @namespace         https://soulsign.inu1255.cn/scripts/233
-// @version           1.0.4
+// @version           1.0.5
 // @author            yi-Xu-0100
 // @loginURL          https://music.163.com/#/login
 // @updateURL         https://soulsign.inu1255.cn/script/yi-Xu-0100/网易云音乐
@@ -13,7 +13,7 @@
  * @file 网易云音乐签到脚本
  * @author yi-Xu-0100
  * @author inu1255
- * @version 1.0.4
+ * @version 1.0.5
  */
 
 /**
@@ -33,12 +33,12 @@
 exports.run = async function () {
   // 移动端签到（10 云贝）
   var point = 0;
-  var { data } = await axios.get('http://music.163.com/api/point/dailyTask?type=0');
+  var { data } = await axios.get('https://music.163.com/api/point/dailyTask?type=0');
   if (data.code === 200) point += data.point;
   else if (data.code != -2) throw data.msg;
 
   // 桌面端签到（5 云贝）
-  var { data } = await axios.get('http://music.163.com/api/point/dailyTask?type=1');
+  var { data } = await axios.get('https://music.163.com/api/point/dailyTask?type=1');
   if (data.code === 200) point += data.point;
   else if (data.code != -2) throw data.msg;
 
