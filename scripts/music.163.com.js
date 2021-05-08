0d38f39eb9e8e58642f7c38a4bfb451cbab06040
yi_Xu <xjy123456120@163.com>
2021-05-08 09:06:41 +0800

    fix(music.163.js): 🐛 fix reward information error
    When the mobile terminal is in the continuous check-in state,
    it will not be misjudged as repeated check-in.

--- scripts/music.163.com.js
+++ scripts/music.163.com.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              网易云音乐
 // @namespace         https://soulsign.inu1255.cn/scripts/233
-// @version           1.0.5
+// @version           1.0.6
 // @author            yi-Xu-0100
 // @loginURL          https://music.163.com/#/login
 // @updateURL         https://soulsign.inu1255.cn/script/yi-Xu-0100/网易云音乐
@@ -13,7 +13,7 @@
  * @file 网易云音乐签到脚本
  * @author yi-Xu-0100
  * @author inu1255
- * @version 1.0.5
+ * @version 1.0.6
  */
 
 /**
@@ -30,25 +30,28 @@
  * @param {string} [updateURL = https://soulsign.inu1255.cn/script/yi-Xu-0100/网易云音乐] - 脚本更新链接
  */
 
-exports.run = async function () {
+const run = async function (param) {
+  if (!(await check(param))) throw '需要登录';
   // 移动端签到（10 云贝）
-  var point = 0;
-  var { data } = await axios.get('https://music.163.com/api/point/dailyTask?type=0');
-  if (data.code === 200) point += data.point;
-  else if (data.code != -2) throw data.msg;
+  const resp = await axios.get('https://music.163.com/api/point/dailyTask?type=0');
+  if (resp.data.code !== 200 && resp.data.code !== -2) throw resp.data.msg;
 
   // 桌面端签到（5 云贝）
-  var { data } = await axios.get('https://music.163.com/api/point/dailyTask?type=1');
-  if (data.code === 200) point += data.point;
-  else if (data.code != -2) throw data.msg;
+  const resp1 = await axios.get('https://music.163.com/api/point/dailyTask?type=1');
+  if (resp1.data.code !== 200 && resp1.data.code !== -2) throw resp.data.msg;
 
-  if (point === 10) return '获得 ' + point + ' 云贝，桌面端已签到';
-  else if (point === 5) return '获得 ' + point + ' 云贝，移动端已签到';
-  else if (point === 15) return '获得 15 云贝';
-  else return '桌面端、移动端均已签到';
+  if (resp.data.code === 200 && resp1.data.code === 200) {
+    return `获得 ${resp.data.point + resp1.data.point} 云贝`;
+  } else if (resp.data.code === -2 && resp1.data.code === 200) {
+    return `获得 ${resp1.data.point} 云贝，移动端已签到`;
+  } else if (resp.data.code === 200 && resp1.data.code === -2) {
+    return `获得 ${resp.data.point} 云贝，桌面端已签到`;
+  } else return '桌面端、移动端均已签到';
 };
 
-exports.check = async function () {
-  var { data } = await axios.get('https://music.163.com/');
-  return !/GUser={}/.test(data);
+const check = async function (param) {
+  const resp = await axios.get('https://music.163.com/');
+  return !/GUser={}/.test(resp.data);
 };
+
+module.exports = { run, check };
