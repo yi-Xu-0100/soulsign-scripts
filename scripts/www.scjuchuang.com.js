312f0dead3052ed903e835161eb93a3bfa771722
yi_Xu <yi_Xu@yixuju.cn>
2021-08-17 10:55:29 +0800

    fix: 🐛 update www.scjuchuang.com.js to v1.0.8
    update check function
    fix pwd description
    
    close #37

--- scripts/www.scjuchuang.com.js
+++ scripts/www.scjuchuang.com.js
@@ -1,20 +1,20 @@
 // ==UserScript==
 // @name              聚创
 // @namespace         https://soulsign.inu1255.cn/scripts/186
-// @version           1.0.7
+// @version           1.0.8
 // @author            yi-Xu-0100
 // @loginURL          https://www.scjuchuang.com/login/index
 // @updateURL         https://soulsign.inu1255.cn/script/yi-Xu-0100/聚创
 // @expire            900000
 // @domain            www.scjuchuang.com
 // @param            name 账户
-// @param            pwd 加密密码
+// @param            pwd 密码
 // ==/UserScript==
 
 /**
  * @file 聚创签到脚本
  * @author yi-Xu-0100
- * @version 1.0.7
+ * @version 1.0.8
  */
 
 /**
@@ -30,27 +30,27 @@
  * @param {string} [loginURL = https://www.scjuchuang.com/login/index] - 登录链接
  * @param {string} [updateURL = https://soulsign.inu1255.cn/script/yi-Xu-0100/聚创] - 脚本更新链接
  * @param {string} name - 账户
- * @param {string} pwd - 加密密码
+ * @param {string} pwd - 密码
  */
 
 let run = async function (param) {
   if (!(await check(param))) throw '需要登录';
-  var resp = await axios.post('https://www.scjuchuang.com/api/signin', null, {
+  let resp = await axios.post('https://www.scjuchuang.com/api/signin', null, {
     headers: {
       Origin: 'https://www.scjuchuang.com',
       Referer: 'https://www.scjuchuang.com/intergralSigin'
     }
   });
   if (/签到成功/.test(resp.data.msg))
-    return `今日获得 ${resp.data.data.integral}积分，已连续签到 ${resp.data.data.continue_count} 天`;
+    return `今日获得 ${resp.data.data.integral} 积分，已连续签到 ${resp.data.data.continue_count} 天`;
   if (/今日已签到/.test(resp.data.msg)) return '重复签到';
   if (/请登录/.test(resp.data.msg)) throw "需要登录";
   throw resp.data;
 };
 
 let check = async function (param) {
-  var resp = await axios.get('https://www.scjuchuang.com/index');
-  if (!/进入会员中心/.test(resp.data)) {
+  let resp = await axios.get('https://www.scjuchuang.com/membercenter');
+  if (!/我的余额/.test(resp.data)) {
     await axios.post(
       'https://www.scjuchuang.com/api/login',
       { username: param.name, password: param.pwd },
@@ -62,8 +62,8 @@ let check = async function (param) {
         }
       }
     );
-    let resp = await axios.get('https://www.scjuchuang.com/index');
-    return /进入会员中心/.test(resp.data);
+    let resp = await axios.get('https://www.scjuchuang.com/membercenter');
+    return /我的余额/.test(resp.data);
   } else return true;
 };
 
